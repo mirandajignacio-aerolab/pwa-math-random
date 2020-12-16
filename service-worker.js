01ab93a8dab9e2d35e5a9dff3a47b7a10f1548c7
Miranda J. Ignacio <mirandajignacio@gmail.com>
2020-12-16 14:52:33 -0300

    miau

--- service-worker.js
+++ service-worker.js
@@ -36,12 +36,9 @@ registerRoute(
 
 // Cache CSS, JS, and Web Worker requests with a Stale While Revalidate strategy
 registerRoute(
-  // Check to see if the request's destination is style for stylesheets, script for JavaScript, or worker for web worker
   ({ request }) =>
-    request.destination === 'style' ||
-    request.destination === 'script' ||
+    request.destination === 'style' || request.destination === 'script' ||
     request.destination === 'worker',
-  // Use a Stale While Revalidate caching strategy
   new StaleWhileRevalidate({
     // Put all cached files in a cache named 'assets'
     cacheName: 'assets',
